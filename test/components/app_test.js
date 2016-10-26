4bb3eaa4d38dd3dea5068fcfbf9ec950a3925d48
samnags <sam.nagourney@gmail.com>
2016-10-25 21:54:02 -0400

    first test

---
+++ test/components/app_test.js
@@ -0,0 +1,24 @@
+// importing things from
+import {renderComponent, expect } from '../test_helper';
+
+// importing component we're testing
+import App from '../../src/components/app';
+
+
+// describe takes first argument of string and second argument of function
+describe ('App', () => {
+
+  // it also takes first argument of string and second of function
+  it('shows the correct text', () => {
+
+    // create an instance of App
+    const component = renderComponent(App);
+
+    // passing what we're testing into expect
+    expect(component).to.contain('React simple starter');
+
+  });
+
+});
+
+// by wrapping specs within functions, Mocha execute tests and not worry about  handle throwing errors
